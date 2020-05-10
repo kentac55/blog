681309e1494fcbe27e833716b79f935bf3e0ff5e
kentac55 <dev.kentac55@gmail.com>
2020-05-10 18:02:24 +0900

    import from unix-bio

---
+++ next.config.js
@@ -0,0 +1,69 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.(md|mdx)?$/,
+  options: {
+    rehypePlugins: [require('@mapbox/rehype-prism'), require('rehype-join-line')],
+  },
+})
+
+const nextConfig = {
+  target: 'serverless',
+  
+  pageExtensions: ['jsx', 'js', 'mdx', 'md', 'ts', 'tsx'],
+  
+  cssModules: true,
+  
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: '[local]___[hash:base64:5]',
+  },
+  
+  env: {
+    VERSION: require('./package.json').version,
+  },
+  
+  webpack(config) {
+    config.resolve.modules.push(__dirname)
+    return config
+  },
+  
+  experimental: {
+    reactRefresh: true,
+
+    redirects() {
+      return [
+        {
+          source: '/blog/others/:path*',
+          permanent: true,
+          destination: '/posts/:path*'
+        },
+        {
+          source: '/blog/others/:path*/',
+          permanent: true,
+          destination: '/posts/:path*'
+        },
+        {
+          source: '/blog/js/:path*',
+          permanent: true,
+          destination: '/posts/:path*'
+        },
+        {
+          source: '/blog/js/:path*/',
+          permanent: true,
+          destination: '/posts/:path*'
+        },
+        {
+          source: '/blog/proxy/:path*',
+          permanent: true,
+          destination: '/posts/:path*'
+        },
+        {
+          source: '/blog/proxy/:path*/',
+          permanent: true,
+          destination: '/posts/:path*'
+        },
+      ]
+    },
+  },
+}
+
+module.exports = withMDX(nextConfig)
